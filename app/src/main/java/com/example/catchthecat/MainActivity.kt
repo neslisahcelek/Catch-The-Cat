cb5abf1c375975d90588f37bb440d9383a5eaf93
Neslisah <neslisah.celek@outlook.com>
2023-04-03 12:33:48 +0300

    Add movement to cats

--- app/src/main/java/com/example/catchthecat/MainActivity.kt
+++ app/src/main/java/com/example/catchthecat/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.catchthecat.databinding.ActivityMainBinding
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,13 +18,48 @@ class MainActivity : AppCompatActivity() {
     var handler: Handler = Handler()
 
     var score=0
-    var time=5000
+    var time=25000
 
+    val catList = ArrayList<ImageView>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        catVisible()
         time()
+    }
+    fun catVisible(){
+        catList.add(binding.imageViewCat)
+        catList.add(binding.imageViewCat2)
+        catList.add(binding.imageViewCat3)
+        catList.add(binding.imageViewCat4)
+        catList.add(binding.imageViewCat5)
+        catList.add(binding.imageViewCat6)
+        catList.add(binding.imageViewCat7)
+        catList.add(binding.imageViewCat8)
+        catList.add(binding.imageViewCat9)
+        catList.add(binding.imageViewCat10)
+        catList.add(binding.imageViewCat11)
+        catList.add(binding.imageViewCat12)
+        catList.add(binding.imageViewCat13)
+        catList.add(binding.imageViewCat14)
+        catList.add(binding.imageViewCat15)
+        catList.add(binding.imageViewCat16)
+
+        for (cat in catList){
+            cat.visibility=View.INVISIBLE
+        }
+        val random = Random()
+        var index = random.nextInt(16)
+            runnable = object : Runnable{
+            override fun run() {
+                catList.get(index).visibility=View.INVISIBLE
+                index = random.nextInt(16)
+                catList.get(index).visibility=View.VISIBLE
+                handler.postDelayed(this,1000)
+            }
+        }
+        handler.post(runnable)
     }
     fun increaseScore(view:View){
         binding.textViewScore.text="Score: ${++score}"
@@ -34,6 +72,7 @@ class MainActivity : AppCompatActivity() {
                 binding.textViewTime.text="Time: ${millisUntilFinished/1000}"
             }
             override fun onFinish() {
+                handler.removeCallbacks(runnable)
                 alert()
             }
         }.start()
@@ -43,7 +82,7 @@ class MainActivity : AppCompatActivity() {
         alert.setTitle("Time is Over!")
         alert.setMessage("Restart the game?")
         alert.setNegativeButton("No"){dialog, which ->
-            Toast.makeText(applicationContext,"Game over.",Toast.LENGTH_LONG).show()
+            //Toast.makeText(applicationContext,"Game over.",Toast.LENGTH_LONG).show()
             binding.buttonRestart.visibility=View.VISIBLE
         }
         alert.setPositiveButton("Yes"){dialog, which ->
